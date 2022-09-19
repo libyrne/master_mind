be63ff5fcc818576203f3ffeadb0b18cf79958e9
Lisa Byrne <93141618+libyrne@users.noreply.github.com>
2022-09-19 11:29:49 -0400

    Update master_mind.h

--- master_mind.h
+++ master_mind.h
@@ -8,28 +8,26 @@ using namespace std;
 class code{
     public:
         code(int n, int m); // constructor
-        void getGuess();
         int checkCorrect();
         int checkIncorrect();
     private:
         vector<int> _secret_code;
-        vector<int> _guess
 };
 
-code::code(int n, int m){
+code::code(int n, int m)
+{
     for (int i = 0; i < n; i++){
         _secret_code.push_back(rand()%m);
 }
 
-void code::getGuess(vector<int> guess){
     
-}
-    
-int code::checkCorrect(){
+int code::checkCorrect()
+{
     
 }
 
-int code::checkIncorrect(){
+int code::checkIncorrect()
+{
     
 }
 
