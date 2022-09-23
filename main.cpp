36231fa0f901a5cca6f5744de1e7ff2da49098dd
Lisa Byrne <byrne.li@northeastern.edu>
2022-09-23 13:29:46 -0400

    most recent version of code

---
+++ main.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Kaitlyn O’Flaherty on 9/15/22.
+//
+// file: master_mind_main
+//
+// description: main function for master_mind A
+//
+
+#include "master_mind.h"
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    //Ask player for length and range
+    int n, m;
+    cout << "Please enter the length of the sequence: " << endl;
+    cin >> n;
+    cout << "Please enter the range of the sequence: " << endl;
+    cin >> m;
+
+    // Use length and range to instatiate code object
+    code sc(n, m);
+       
+    //Generate secret code
+    sc.generateSecret();
+
+    code guess1(n,m);
+    code guess2(n,m);
+    code guess3(n,m);
+    
+    guess1.getGuess();
+    guess2.getGuess();
+    guess3.getGuess();
+    
+    //Print results of checks
+    sc.checkCorrect(guess1);
+    sc.checkCorrect(guess2);
+    sc.checkCorrect(guess3);
+
+    sc.checkIncorrect(guess1);
+    sc.checkIncorrect(guess2);
+    sc.checkIncorrect(guess3);                                                                          
+    
+    return 0;
+}
+
--- master_mind.h
+++ master_mind.h
@@ -2,37 +2,92 @@
 #define CODE_H_
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class code{
     public:
         code(int n, int m); // constructor
-        int checkCorrect();
-        int checkIncorrect();
+        void getGuess();
+        void generateSecret();
+        int checkCorrect(code guess);
+        int checkIncorrect(code guess); 
     private:
-        vector<int> _code;
+        vector<int> _sequence;
         int _length;
         int _range;
+        
 };
 
+// Constructor for a code object with a "_sequence" vector v, "_length" n, and "_range" m
 code::code(int n, int m)
 {
+    _sequence.resize(n);
     _length = n;
     _range = m;
+}
+
+// Function to generate a secret code with the code object 
+// The secret code is a "_sequence" vector of length "_length" with random values from range [0, "_range"-1]
+void code::generateSecret()
+{
     for (int i = 0; i < _length; i++){
-        _code.push_back(rand()%_range);
+        _sequence.push_back(rand() % _range);
+    }
+    cout << "The secret code is: ";
+        for (int x : _sequence)
+            cout << x << " ";
 }
 
-    
-int code::checkCorrect()
-{
-    
+void code::getGuess(){
+    cout << "\nPlease enter your guess code: " << endl;
+        for(int i = 0 ; i < _length ; i++)
+            {
+                int guess;
+                cin >> guess;
+                if (guess < _range){
+                    _sequence.push_back(guess);
+                }
+                else{
+                    cout << "Guess is out of range, please reenter." << endl;
+                    i--;
+                }
+            }
+        cout << "The guess code is: " <<endl;
+        for (int y : _sequence)
+            cout << y << " ";
+
 }
 
-int code::checkIncorrect()
+// Function to check the number of matching values at matching locations 
+// between a secret code vector and a guess vector
+int code::checkCorrect(code guess)
 {
-    
+    int correctCount = 0;
+    for (int sc_i = 0; sc_i < _length; sc_i++){
+        if (_sequence[sc_i] == guess._sequence[sc_i]){
+            correctCount++;
+        }
+    }
+    return correctCount;
 }
+
+int code::checkIncorrect(code guess)
+{
+    int count = 0;
+    for (int sc_i = 0; sc_i < _length; sc_i++){
+        for (int g_i = 0; g_i < _length; g_i++){
+            if (g_i != sc_i){
+                if (_sequence[sc_i] == guess._sequence[g_i]){
+                    count += 1;
+                    guess._sequence[g_i] = _range; // Set counted values out of range, so they won't be double counted
+                }
+            }
+        }
+    }
+    return count;
+}
+
 
 #endif
